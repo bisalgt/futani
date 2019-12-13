77e27eed61df71571162eecbaf7cbd95434f6ef1
Bishal <bisalgt@gmail.com>
2019-12-13 14:11:39 +0545

    account login/signup finished with added pagination to gallery list page.

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.home.views import gallery, feedback
+from apps.home.views import gallery, feedback, GalleryListView
 
 urlpatterns = [
 
-    path("form/", gallery, name="gallery"),
+    path("gallery/", GalleryListView.as_view(), name="gallery_list"),
+    path("gallery_form/", gallery, name="gallery_form"),
     path("feedback_form/", feedback,  name="feedback_form"),
 
 ]
