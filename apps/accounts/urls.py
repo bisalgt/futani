373072e55e6bf9054ecf3c372e0b3eac3c30c7ac
Bishal <bisalgt@gmail.com>
2019-12-15 15:05:59 +0545

    new html created for user update

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from apps.accounts.views import SignUpView
+from apps.accounts.views import SignUpView, UserUpdateView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("update_user/<int:pk>/", UserUpdateView.as_view(), name="update_user"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,9 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
-from .forms import CustomUserCreationForm
+from apps.accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from apps.accounts.models import User
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+class UserUpdateView(UpdateView):
+    form_class = CustomUserChangeForm
+    queryset = User.objects.all()
+    success_url = reverse_lazy("login")
+    template_name = "accounts/update.html"
