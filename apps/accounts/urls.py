a6d1d8fb584e2aa2969d836736184942ef47eec4
Bishal <bisalgt@gmail.com>
2019-12-15 16:27:11 +0545

    adding user to navbar

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
-from apps.accounts.views import SignUpView, UserUpdateView
+from apps.accounts.views import SignUpView, UserUpdateView, user_detail
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("update_user/<int:pk>/", UserUpdateView.as_view(), name="update_user"),
+    path("user_detail/<int:id>/", user_detail, name="user_detail"),
     path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,17 +1,26 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from apps.accounts.forms import CustomUserCreationForm, CustomUserChangeForm
 from apps.accounts.models import User
 
-class SignUpView(CreateView):
+class SignUpView(LoginRequiredMixin, CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
 
-class UserUpdateView(UpdateView):
+class UserUpdateView(LoginRequiredMixin, UpdateView):
     form_class = CustomUserChangeForm
     queryset = User.objects.all()
     success_url = reverse_lazy("login")
     template_name = "accounts/update.html"
+
+@login_required
+def user_detail(request, id):
+    user = get_object_or_404(User, id=id)
+    context = {'user':user}
+    return render(request, 'accounts/user_detail.html', context)
