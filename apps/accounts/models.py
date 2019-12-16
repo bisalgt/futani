ac229e52508bf91982ff1cfda59bbb59483e7afc
Bishal <bisalgt@gmail.com>
2019-12-16 11:36:55 +0545

    link in navbarfixed and used a absolute url

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=12, null=True, blank=True, unique=True)
     address = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(upload_to='profilepics', null=True, blank=True, verbose_name="Profile Picture")
+
+    def get_absolute_url(self):
+        return reverse("user_detail", kwargs={"id": self.id})
+    
